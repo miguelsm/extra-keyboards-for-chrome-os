0429df5a76e3cf047575cde4b7a1a2ca08aa05f8
miguelsm <miguelsm@penguin>
2022-07-29 23:06:23 +0100

    Make capslockremap use Colemak layout

--- capslockremap/background.js
+++ capslockremap/background.js
@@ -13,17 +13,48 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-var contextID = -1;
+var contextID = 0;
 var lastRemappedKeyEvent = undefined;
 var ctrlKey = false;
 
-chrome.input.ime.onFocus.addListener(function(context) {
-  contextID = context.contextID;
-});
+var lut = {
+  "KeyQ": [ "q", "Q" ],
+  "KeyW": [ "w", "W" ],
+  "KeyE": [ "f", "F"],
+  "KeyR": [ "p", "P" ],
+  "KeyT": [ "g", "G" ],
+  "KeyY": [ "j", "J" ],
+  "KeyU": [ "l", "L" ],
+  "KeyI": [ "u", "U" ],
+  "KeyO": [ "y", "Y" ],
+  "KeyP": [ ";", ":" ],
+  "KeyA": [ "a", "A" ],
+  "KeyS": [ "r", "R" ],
+  "KeyD": [ "s", "S" ],
+  "KeyF": [ "t", "T" ],
+  "KeyG": [ "d", "D" ],
+  "KeyH": [ "h", "H" ],
+  "KeyJ": [ "n", "N" ],
+  "KeyK": [ "e", "E" ],
+  "KeyL": [ "i", "I" ],
+  "KeyZ": [ "z", "Z" ],
+  "KeyX": [ "x", "X" ],
+  "KeyC": [ "c", "C" ],
+  "KeyV": [ "v", "V" ],
+  "KeyB": [ "b", "B" ],
+  "KeyN": [ "k", "K" ],
+  "KeyM": [ "m", "M" ],
+};
 
-chrome.input.ime.onBlur.addListener(function(context) {
-  contextID = -1;
-});
+chrome.input.ime.onFocus.addListener(
+    function(context) {
+      contextID = context.contextID;
+    }
+);
+
+chrome.input.ime.onBlur.addListener(() => {
+  contextID = 0;
+})
 
 function isCapsLock(keyData) {
    return (keyData.code == "CapsLock");
@@ -55,7 +86,8 @@ chrome.input.ime.onKeyEvent.addListener(
         ctrlKey = keyData.ctrlKey;
         keyData.capsLock = false;
         chrome.input.ime.sendKeyEvents({"contextID": contextID, "keyData": [keyData]});
-        lastRemappedKeyEvent = keyData;                                                 handled = true;
+        lastRemappedKeyEvent = keyData;
+        handled = true;
       } else if (ctrlKey) {
         keyData.ctrlKey = ctrlKey;
         keyData.capsLock = false;
@@ -63,10 +95,28 @@ chrome.input.ime.onKeyEvent.addListener(
         lastRemappedKeyEvent = keyData;
         handled = true;
       } else if (keyData.capsLock) {
-	keyData.capsLock = false;
+        keyData.capsLock = false;
         chrome.input.ime.sendKeyEvents({"contextID": contextID, "keyData": [keyData]});
         lastRemappedKeyEvent = keyData;
         handled = true;
+      } else if (keyData.type == "keydown") {
+        if (lut[keyData.code]) {
+          let shifted = keyData.capsLock != keyData.shiftKey;
+          let emit = lut[keyData.code][+shifted];
+
+          if (emit != null && contextID != 0) {
+            chrome.input.ime.commitText({
+              "contextID": contextID,
+              "text": emit,
+            }, () => {
+              if (chrome.runtime.lastError) {
+                console.error('Error committing text:', chrome.runtime.lastError);
+                return;
+              }
+            });
+          }
+          handled = true;
+        }
       }
       
       return handled;
